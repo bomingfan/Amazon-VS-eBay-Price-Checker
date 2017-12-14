ce97367f656cd1caf6f3661b0bbd7aae6b020190
buzzhaha <boming.fan@gmail.com>
2017-12-14 12:39:47 -0800

    button-work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,74 +9,73 @@ var config = {
 };
 firebase.initializeApp(config);
 
-// Ebay API and AJAX items
-$("#item-search").on("click", function (event) {
 
-  event.preventDefault();
+function showInfo(item) {
 
-  $("#ebay").empty();
+   // Ebay API and AJAX items
+    event.preventDefault();
+  
+    $("#ebay").empty();
+  
+    var appKey = "DrewZele-priceche-PRD-c5d8a3c47-8e4e1b10";
+    // item = $("#enter-product").val().trim();
+    var queryURL = "https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=" + appKey + "&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" + item + "&paginationInput.entriesPerPage=10&GLOBAL-ID=EBAY-US&siteid=0"
+  
+    console.log(queryURL);
+    $.ajax({
+      dataType: "jsonp",
+      url: queryURL,
+      method: "GET"
+    }).done(function (response) {
+      console.log(response);
+  
+      var ebayItem = response.findItemsByKeywordsResponse[0].searchResult[0]
+  
+      for (i = 0; i < 10; i++) {
+  
+        $("#ebay").append("<h4>" + ebayItem.item[i].title["0"] + "</h4>");
+        $("#ebay").append("<img src = " + ebayItem.item[i].galleryURL["0"] + " class='img-thumbnail'>");
+        $("#ebay").append("<p> $" + ebayItem.item[i].sellingStatus["0"].currentPrice["0"].__value__ + "</p>");
+        $("#ebay").append("<p><a href = " + ebayItem.item[i].viewItemURL["0"] + ">Click here for eBay page</a></p><br>")
+      }
+  
+    }).catch(function (error) {
+      console.log("status", error.status);
+      console.log(error);
+    });
+  
+    // Walmart Ajax Call
+  
+    $("#walmart").empty();
+  
+    var Key = "m293p2wqduce6kc3xusuz4ug";
+    queryURL = "https://api.walmartlabs.com/v1/search?apiKey=" + Key + "&query=" + item;
+  
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      // contentType: 'text/plain',
+      // xhrFields: {withCredentials: false},
+      dataType: "jsonp",
+      // Headers: {'Access-Control-Allow-Origin': '*'}
+    }).done(function (res) {
+      console.log(res);
+  
+      for (i = 0; i < res.items.length; i++) {
+  
+        var newDiv = $("<div>");
+        newDiv.html("<h4>" + res.items[i].name + "</h4>");
+        newDiv.append("<a href = " + res.items[i].productUrl + " target='_blank'><img src = " + res.items[i].imageEntities["0"].mediumImage + " class='img-thumbnail'></a>");
+        newDiv.append("<p>$" + res.items[i].salePrice + "</p>");
+        newDiv.append("<a class='button' href = " + res.items[i].addToCartUrl + " target='_blank'>Add To Cart</a>");
+        $("#walmart").append(newDiv);
+  
+      };
+  
+    });
 
-  var appKey = "DrewZele-priceche-PRD-c5d8a3c47-8e4e1b10";
-  var item = $("#enter-product").val().trim();
-  var queryURL = "https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=" + appKey + "&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" + item + "&paginationInput.entriesPerPage=10&GLOBAL-ID=EBAY-US&siteid=0"
+}
 
-  console.log(queryURL);
-  $.ajax({
-    dataType: "jsonp",
-    url: queryURL,
-    method: "GET"
-  }).done(function (response) {
-    console.log(response);
-
-    var ebayItem = response.findItemsByKeywordsResponse[0].searchResult[0]
-
-    for (i = 0; i < 10; i++) {
-
-      $("#ebay").append("<h4>" + ebayItem.item[i].title["0"] + "</h4>");
-      $("#ebay").append("<img src = " + ebayItem.item[i].galleryURL["0"] + " class='img-thumbnail'>");
-      $("#ebay").append("<p> $" + ebayItem.item[i].sellingStatus["0"].currentPrice["0"].__value__ + "</p>");
-      $("#ebay").append("<p><a href = " + ebayItem.item[i].viewItemURL["0"] + ">Click here for eBay page</a></p><br>")
-    }
-
-  }).catch(function (error) {
-    console.log("status", error.status);
-    console.log(error);
-  });
-
-  // Walmart Ajax Call
-
-  $("#walmart").empty();
-
-  event.preventDefault();
-
-  var Key = "m293p2wqduce6kc3xusuz4ug";
-  var product = $("#enter-product").val().trim();
-  queryURL = "https://api.walmartlabs.com/v1/search?apiKey=" + Key + "&query=" + product;
-
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    // contentType: 'text/plain',
-    // xhrFields: {withCredentials: false},
-    dataType: "jsonp",
-    // Headers: {'Access-Control-Allow-Origin': '*'}
-  }).done(function (res) {
-    console.log(res);
-
-    for (i = 0; i < res.items.length; i++) {
-
-      var newDiv = $("<div>");
-      newDiv.html("<h4>" + res.items[i].name + "</h4>");
-      newDiv.append("<a href = " + res.items[i].productUrl + " target='_blank'><img src = " + res.items[i].imageEntities["0"].mediumImage + " class='img-thumbnail'></a>");
-      newDiv.append("<p>$" + res.items[i].salePrice + "</p>");
-      newDiv.append("<a class='button' href = " + res.items[i].addToCartUrl + " target='_blank'>Add To Cart</a>");
-      $("#walmart").append(newDiv);
-
-    };
-
-
-  });
-});
 
 
 // make a new button for them to click on later
@@ -100,6 +99,8 @@ $("#item-search").on("click", function (event) {
 
   // Get the input values
   var productName = $("#enter-product").val().trim();
+
+  showInfo(productName);
 
   // Save the new price in Firebase
   database.ref().push({
@@ -137,14 +138,21 @@ database.ref().orderByChild("dateAdded").limitToLast(3).on("child_added", functi
     $("#buttons button:nth-last-child(4)").remove();
   }
 
-  // console.log($(".buttons:nth-child(4)").val());
+
 
   // Handle the errors
 }, function (errorObject) {
   console.log("Errors handled: " + errorObject.code);
 });
 
+$(document).on("click", "button", function () {
+  
+  // Get the input values
+  var itemName = $(this).attr("data-search");
 
+  showInfo(itemName);
+
+  });
 
 
 
